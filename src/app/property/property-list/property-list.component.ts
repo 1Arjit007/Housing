bee8711091f88081f0c4bb04526ce4779bc921e0
Arjit <1arjit.kesarwani@gmail.com>
2022-09-25 14:43:19 +0530

    property-list and property card updated

--- src/app/property/property-list/property-list.component.ts
+++ src/app/property/property-list/property-list.component.ts
@@ -7,6 +7,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PropertyListComponent implements OnInit {
 
+  Properties:Array<any>=[
+  {
+    "Id":1,
+    "Name":"Birla house",
+    "Type":"House",
+    "Price":1200
+  },
+  {
+    "Id":2,
+    "Name":"Erose Villa",
+    "Type":"House",
+    "Price":14000
+  },
+  {
+    "Id":3,
+    "Name":"White House",
+    "Type":"House",
+    "Price":120000
+  }
+]
   constructor() { }
 
   ngOnInit() {
